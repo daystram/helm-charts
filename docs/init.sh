665171cafaa0c3e0082be9950ba1f1c89a6f379e
Danny August Ramaputra <daystram@gmail.com>
2021-02-11 14:42:51 +1000

    refactor: updated init script

--- docs/init.sh
+++ docs/init.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 REPO="https://charts.daystram.com"
 
-if [ -z $1 ]; then
-    echo "Usage: curl -sfL $REPO/init.sh | sh - [application name]";
-    exit
-fi
-
 if [ -d ".daystram/helm-chart" ]; then
     echo -n "Helm chart already initialized at .daystram/helm-chart, overwrite? [y/N] ";
     read resp;
     if [ "$resp" = "${resp#[Yy]}" ]; then
-        exit
+        exit 1
     fi
+fi
+
+echo -n "Application name: ";
+read APPLICATION_NAME;
+if [ -z "$APPLICATION_NAME" ]; then
+    echo "You have to provide an application name!"
+    exit 1
+fi
+
+if echo "$APPLICATION_NAME" | grep -q " "; then
+    echo "Invalid application name! Name must not contain spaces."
+    exit 1
 fi
 
 echo "Creating Helm chart for $1..."
@@ -20,6 +27,7 @@ curl -s "$REPO/scaffold.tgz" -o scaffold.tgz
 tar -xzf scaffold.tgz
 rm scaffold.tgz
 
-find .daystram/helm-chart -type f -print0 | xargs -0 sed -i "s/APPLICATION_NAME/$1/g"
+find .daystram/helm-chart -type f -print0 | xargs -0 sed -i "s/APPLICATION_NAME/$APPLICATION_NAME/g"
 
 echo "Chart succesfully created at .daystram/helm-chart"
+exit 0
